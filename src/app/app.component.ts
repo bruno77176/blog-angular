ef89cc114abfa904f75970fe1132cf8b282febf1
Bruno Moise <b.moise@outlook.com>
2019-04-07 17:41:42 +0200

    tp angular ocr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  title1 = 'blog-angular';
+  post1 = new Post("Mon premier Post", 
+  "du contenu de grande qualité pour un TP de qualité");
+  post2 = new Post("Mon deuxième Post", 
+  " aujourd'hui je vais apprendre Angular, c'est rigolo");
+  post3 = new Post("Un autre Post", 
+  " c'est rigolo mais c'est salop! Et il faut d'abord connaitre TypeScript!");
+
+  posts = [ this.post1, this.post2, this.post3];
+}
+
+class Post {
+  title : string;
+  content : string;
+  loveIts : number;
+  created_at : Date;
+
+  constructor(title, content) {
+    this.title = title;
+    this.content = content;
+    this.loveIts=0;
+    this.created_at= new Date();
+  }
 }
